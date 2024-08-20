e52ca673a51e8ce37525cb3c92fba88bab9fb24c
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-08-20 13:49:36 +0100

    add html to form

--- src/ProjectForm.tsx
+++ src/ProjectForm.tsx
@@ -1,5 +1,21 @@
 
 
 export const ProjectForm = () => {
-    return <p>hello</p>
+    return (
+        <form action="">
+            <label htmlFor="">Project Name</label>
+            <input type="text" />
+            <label htmlFor="">Description</label>
+            <input type="text" />
+            <label htmlFor="">Date</label>
+            <input type="date" />
+            <label htmlFor="">Project Type</label>
+            <select name="" id="">
+                <option value="">none</option>
+                <option value="Business">Business</option>
+                <option value="Pleasure">Pleasure</option>
+            </select>
+            <button>Create Project</button>
+        </form>
+    )
 }
