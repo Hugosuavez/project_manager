19183fb89d29749e6fae0d10d6fb05923b39e0ac
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-08-20 15:57:46 +0100

    format with prettier

--- src/ProjectDetails.tsx
+++ src/ProjectDetails.tsx
@@ -1,28 +1,26 @@
 import { IProject } from "./models/Project";
 
-
 export const ProjectDetails = ({
-    project,
-    setProject,
-  }: {
-    project: IProject;
-    setProject: React.Dispatch<React.SetStateAction<IProject | null>>;
-  }) => {
+  project,
+  setProject,
+}: {
+  project: IProject;
+  setProject: React.Dispatch<React.SetStateAction<IProject | null>>;
+}) => {
+  const createNewProject = () => {
+    setProject(null);
+  };
 
-    const createNewProject = () => {
-        setProject(null)
-    }
-
-    return (
-        <section>
-    <p>Your Projects</p>
-    <button onClick={createNewProject}>Create New Project</button>
-    <article>
+  return (
+    <section>
+      <p>Your Projects</p>
+      <button onClick={createNewProject}>Create New Project</button>
+      <article>
         <p>Project Name{project.name}</p>
         <p>Description: {project.description}</p>
         <p>Type: {project.projectType}</p>
         <p>Start date: {project.startDate}</p>
-    </article>
+      </article>
     </section>
-)
-}
+  );
+};
